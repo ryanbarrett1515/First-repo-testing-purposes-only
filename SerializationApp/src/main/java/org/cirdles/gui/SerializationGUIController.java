05e288e94d1c2eab47e09410a28679a603a9b13f
Ryan Barrett <barrettra@g.cofc.edu>
2018-01-24 18:12:47 -0500

    ui work in progress

--- SerializationApp/src/main/java/org/cirdles/gui/SerializationGUIController.java
+++ SerializationApp/src/main/java/org/cirdles/gui/SerializationGUIController.java
@@ -5,7 +5,6 @@
  */
 package org.cirdles.gui;
 
-import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -13,12 +12,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuButton;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import org.cirdles.fileinputstuff.Person;
-import org.cirdles.fileinputstuff.XMLSerialization;
 
 /**
  * FXML Controller class
@@ -28,16 +28,7 @@ import org.cirdles.fileinputstuff.XMLSerialization;
 public class SerializationGUIController implements Initializable {
 
     @FXML
-    private MenuButton Menu;
-    @FXML
-    private MenuItem MenuItemDeserialize;
-    @FXML
-    private MenuItem MenuItemShowCurrent;
-    @FXML
-    private MenuItem MenuItemSerialize;
-    @FXML
     private AnchorPane RootAnchorPane;
-    @FXML
     private TextField TextFieldPeople;
     @FXML
     private TextField FirstName;
@@ -47,9 +38,35 @@ public class SerializationGUIController implements Initializable {
     private TextField Date;
     @FXML
     private Button CreateNewPerson;
+    @FXML
+    private MenuBar MainMenuBar;
+    @FXML
+    private Menu SerializeMenu;
+    @FXML
+    private MenuItem CSVSerialize;
+    @FXML
+    private MenuItem BinarySerialize;
+    @FXML
+    private MenuItem JDOMSerialize;
+    @FXML
+    private MenuItem XStreamSerialize;
+    @FXML
+    private Menu DeserializeMenu;
+    @FXML
+    private MenuItem CSVDeserialize;
+    @FXML
+    private MenuItem JDOMDeserialize;
+    @FXML
+    private MenuItem XStreamDeserialize;
+    @FXML
+    private Button ClearButton;
+    @FXML
+    private MenuItem BInaryDeserialize;
 
     private ArrayList<Person> list;
     private String text = "";
+    @FXML
+    private TextArea TextAreaPeople;
 
     /**
      * Initializes the controller class.
@@ -60,34 +77,6 @@ public class SerializationGUIController implements Initializable {
     }
 
     @FXML
-    private void DeserializeOnAction(ActionEvent event) {
-        try {
-            File file = new File("XML/people.xml");
-            list = XMLSerialization.getList(file);
-        } catch (Exception e) {
-            TextFieldPeople.setText("Something went wrong.");
-        }
-    }
-
-    @FXML
-    private void ShowCurrentOnAction(ActionEvent event) {
-        TextFieldPeople.setText("Current People");
-        for (int i = 0; i < list.size(); i++) {
-            TextFieldPeople.appendText(list.get(i).personToString());
-        }
-    }
-
-    @FXML
-    private void SerializeOnAction(ActionEvent event) {
-        try {
-            File file = new File("XML/people.xml");
-            XMLSerialization.makeList(list, file);
-        } catch (Exception e) {
-            TextFieldPeople.setText("Something went wrong.");
-        }
-    }
-
-    @FXML
     private void CreateNewPersonOnAction(ActionEvent event) {
         list.add(new Person(FirstName.getText(), LastName.getText(), Date.getText()));
         FirstName.clear();
@@ -95,4 +84,41 @@ public class SerializationGUIController implements Initializable {
         Date.clear();
     }
 
+    @FXML
+    private void CSVSerialize(ActionEvent event) {
+    }
+
+    @FXML
+    private void BinarySerialize(ActionEvent event) {
+    }
+
+    @FXML
+    private void JDOMSerialize(ActionEvent event) {
+    }
+
+    @FXML
+    private void XStreamSerialize(ActionEvent event) {
+    }
+
+    @FXML
+    private void CSVDeserialize(ActionEvent event) {
+    }
+
+    @FXML
+    private void XStreamDeserialize(ActionEvent event) {
+    }
+
+    @FXML
+    private void ClearOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void BinaryDeserialize(ActionEvent event) {
+    }
+
+    @FXML
+    private void JDOMDeserialize(ActionEvent event) {
+    }
+
 }
+//something something something something something something something something
